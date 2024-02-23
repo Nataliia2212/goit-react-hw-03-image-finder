6f5685001494e611d40661a6655b3ce3636f0c75
Nataliia Plokhotnyk <shmilo.nataliya@gmail.com>
2024-02-23 11:32:39 +0200

    all

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,18 @@
+import { Audio } from 'react-loader-spinner'
+import React from 'react'
+
+export default function Loader() {
+  return (
+    <div><Audio
+  height="80"
+  width="80"
+  radius="9"
+  color="green"
+  ariaLabel="three-dots-loading"
+  wrapperStyle
+  wrapperClass
+/></div>
+  )
+}
+
+
